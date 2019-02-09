cccd79bf4f4e28fd98b87178b95a415e7d73de84
ameyabhope <ameya.bhope@gmail.com>
2019-02-09 15:26:51 -0500

    Added classification models, cross validation and export to csv

--- Load_data.py
+++ Load_data.py
@@ -22,29 +22,52 @@ def Read_Files(data_type,path,sentiment):
 train = pd.concat([train_pos,train_neg])
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(train.iloc[:,0], train.iloc[:,1], test_size = 0.2, random_state =10)
+X_train, X_val, y_train, y_val = train_test_split(train.iloc[:,0], train.iloc[:,1], test_size = 0.2, random_state =10)
 
 from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer().fit(X_train)
 X_train_counts = count_vect.transform(X_train)
-X_test_counts = count_vect.transform(X_test)
+X_val_counts = count_vect.transform(X_val)
+X_test_counts = count_vect.transform(test.iloc[:,0])
+
 
 from sklearn.feature_extraction.text import TfidfTransformer
 tfidf_transformer = TfidfTransformer().fit(X_train_counts)
 X_train_tfidf = tfidf_transformer.transform(X_train_counts)
+X_val_tfidf = tfidf_transformer.transform(X_val_counts)
 X_test_tfidf = tfidf_transformer.transform(X_test_counts)
 
 from sklearn.preprocessing import Normalizer
 
 normalizer_tranformer = Normalizer().fit(X=X_train_tfidf)
 X_train_normalized = normalizer_tranformer.transform(X_train_tfidf)
+X_val_normalized = normalizer_tranformer.transform(X_val_tfidf)
 X_test_normalized = normalizer_tranformer.transform(X_test_tfidf)
 
-from sklearn.naive_bayes import MultinomialNB
-clf = MultinomialNB().fit(X_train_normalized, y_train)
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+for c in [0.01, 0.05, 0.25, 0.5, 1]:
+        lr = LogisticRegression()
+        lr.fit(X_train_normalized, y_train)
+        print ("Accuracy for C=%s: %s" 
+           % (c, accuracy_score(y_test, lr.predict(X_val_normalized))))
 
-from sklearn import metrics
-y_pred = clf.predict(X_test_normalized)
-print(metrics.classification_report(y_test, y_pred,
-    target_names=newsgroups.target_names))
+from sklearn.svm import LinearSVC
+for c in [0.01, 0.05, 0.25, 0.5, 1]:
+        svc = LinearSVC()
+        svc.fit(X_train_normalized, y_train)
+        print ("Accuracy for C=%s: %s" 
+           % (c, accuracy_score(y_test, svc.predict(X_val_normalized))))      
+
+from sklearn.naive_bayes import GaussianNB
+for c in [0.01, 0.05, 0.25, 0.5, 1]:
+        gnb = GaussianNB()
+        gnb.fit(X_train_normalized, y_train)
+        print ("Accuracy for C=%s: %s" 
+           % (c, accuracy_score(y_test, gnb.predict(X_val_normalized))))  
+
+a = []
+a = pd.DataFrame(clf.predict(X_test_normalized))
+
+a.to_csv('Sub.csv',index=True)
